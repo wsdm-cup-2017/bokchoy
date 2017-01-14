b9b0bf6bf3a7902970efeb42a487ee9b509d8c51
scissorsy <scissorsy@163.com>
2017-01-14 23:45:14 +0800

    add data path

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # bokchoy
 The Bokchoy Triple Scorer
+Our code sharing contains 2 part, the first is scorers implementation, the second is some data we used.
+
+Folder 'scorers' contains all methods we have implemented in WSDM Cup 2017
+word_classification.py : Word Classification
+word_counting.py : Word Counting
+word_mle.py : Word MLE
+ensemble.py : ensemble method
+refinement.py : Refine by Trigger Word Detection
+
+Folder 'path' is the path of data, and the data root path is configured in conf.py.
--- path/wsdm.py
+++ path/wsdm.py
@@ -10,3 +10,36 @@ professions = os.path.join(data_root, folder, 'profession.list')
 nationality__train = os.path.join(data_root, folder, 'nationality.train')
 profession__train = os.path.join(data_root, folder, 'profession.train')
 wiki_sentences = os.path.join(data_root, folder, 'wiki-sentences')
+
+person2id__dict = os.path.join(data_root, folder, 'person2id.dict')
+id2person__dict = os.path.join(data_root, folder, 'id2person.dict')
+id2prof__dict = os.path.join(data_root, folder, 'id2prof.dict')
+person_profs__dict = os.path.join(data_root, folder, 'person_profs_in_wsdm.dict')
+prof_persons__dict = os.path.join(data_root, folder, 'prof_persons_wsdm.dict')
+pos_prof_persons_filter__dict = os.path.join(data_root, folder, 'pos_profs_person_filter.dict')
+neg_prof_persons_filter__dict = os.path.join(data_root, folder, 'neg_profs_person_filter.dict')
+pos_nation_persons_triples_with_one__dict = os.path.join(data_root, folder, 'pos_nation_persons_triple.dict')
+neg_nation_persons_triple_all__dict = os.path.join(data_root, folder, 'neg_nation_person_triple_all.dict')
+nation_persons__dict = os.path.join(data_root, folder, 'nation_persons.dict')
+person_nations__dict = os.path.join(data_root, folder, 'person_nations.dict')
+pname_popularity__dict = os.path.join(data_root, folder, 'pname_popularity.dict')
+nationalities__set = os.path.join(data_root, folder, 'nationalities.set')
+professions__set = os.path.join(data_root, folder, 'professions.set')
+prof_persons_with_one__dict = os.path.join(data_root, folder, 'persons_in_prof_one_wsdm.dict')
+prof_name2id__dict = os.path.join(data_root, folder, 'prof_name2id_wsdm.dict')
+nation_name2id__dict = os.path.join(data_root, folder, 'nation_name2id_wsdm.dict')
+pos_text_prof__list = os.path.join(data_root, folder, 'pos_text_prof.list')
+description__dict = os.path.join(data_root, folder, 'description.dict')
+nation_id2name__dict = os.path.join(data_root, folder, 'nation_id2nam.dict')
+country_name = os.path.join(data_root, folder, 'country.txt')
+pos_nations_text = os.path.join(data_root, folder, 'pos_nations.dict')
+neg_nations_text = os.path.join(data_root, folder, 'neg_nations.dict')
+person_text = os.path.join(data_root, folder, 'person_text.dict')
+
+
+
+def attr_name2id(task):
+    if task == 'prof':
+        return prof_name2id__dict
+    else:
+        return nation_name2id__dict
